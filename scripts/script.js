1227cecc4fe3e23e4db51206184b9217845e748d
Arseniy <94108412+PoziTronAr@users.noreply.github.com>
2022-03-14 18:29:49 +0200

    Update script.js
    Changed main theme to light

--- scripts/script.js
+++ scripts/script.js
@@ -4,8 +4,8 @@ page.classList.add('dark-theme');
 let themeButton = document.querySelector('.theme-button');
 let myHead = document.querySelector('h1');
 themeButton.onclick = function () {
-  page.classList.toggle('dark-theme');
   page.classList.toggle('light-theme');
+  page.classList.toggle('dark-theme');
 //  myHead.textContent = 'CHEBUREK';
   console.log("Кнопка нажата");
 }
